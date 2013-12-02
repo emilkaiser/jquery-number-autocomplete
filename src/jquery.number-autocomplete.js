8c284431c9ec64be07f8e55aa434e871d03a8623
Emil Kaiser <emilkaiser@gmail.com>
2013-12-02 17:51:39 +0100

    set global options

--- src/jquery.number-autocomplete.js
+++ src/jquery.number-autocomplete.js
@@ -18,8 +18,8 @@
     className: pluginName, // for styling etc
     prefix: '', // something before ..
     suffix: ' kr', // .. and something after
-    unmaskInput: true, // only digits in input field or not 
-    removeFixInput: true, // allow separators but nof prefix or suffix in input field
+    unmaskInput: false, // only digits in input field or not
+    removeFixInput: true, // allow separators but no prefix or suffix in input field
     minDigits: 4, // 1000 to ..
     maxDigits: 8, // .. 10 000 000
     positioning: true // doing some sane positioning below input field
@@ -27,150 +27,138 @@
 
   function NumberAutocomplete (element, options) {
     this.$element = $(element);
-    this.options = $.extend({}, defaults, options);
+    this.options = $.extend({}, defaults, options || {});
     this.$element.on('keyup', $.proxy(this.keyup, this));
   }
 
   NumberAutocomplete.prototype = {
 
-      init: function () {
-        this.$autocomplete = $('<ul></ul>', {class: this.options.className}).hide();
-        $('body').append(this.$autocomplete);
+    init: function () {
+      this.$autocomplete = $('<ul></ul>', {class: this.options.className}).hide();
+      $('body').append(this.$autocomplete);
 
-        this.$autocomplete.on('mousedown', 'li', $.proxy(this.mousedown, this));
-        this.$autocomplete.on('mouseenter', 'li', $.proxy(this.mouseenter, this));
-        this.$autocomplete.on('mouseleave', 'li', $.proxy(this.mouseleave, this));
-        this.$element.on('blur', $.proxy(this.blur, this));
-        this.$element.on('keydown', $.proxy(this.keydown, this));
-        this.$element.on('focus', $.proxy(this.focus, this));
+      this.$autocomplete.on('mousedown', 'li', $.proxy(this.mousedown, this));
+      this.$autocomplete.on('mouseenter', 'li', $.proxy(this.mouseenter, this));
+      this.$autocomplete.on('mouseleave', 'li', $.proxy(this.mouseleave, this));
+      this.$element.on('blur', $.proxy(this.blur, this));
+      this.$element.on('keydown', $.proxy(this.keydown, this));
+      this.$element.on('focus', $.proxy(this.focus, this));
 
-        if (this.options.positioning) {
-          var css = this.$element.offset();
-          css.top += this.$element.height() + 8;
-          css.width = Math.max(this.$element.height(), 100);
-          this.$autocomplete.css(css);
-        }
-      },
+      if (this.options.positioning) {
+        var css = this.$element.offset();
+        css.top += this.$element.height() + 8;
+        css.width = Math.max(this.$element.height(), 100);
+        this.$autocomplete.css(css);
+      }
+    },
 
-      focus: function () {
-        if (this.$autocomplete.html() !== '') {
-          this.$autocomplete.show();
-        }
-      },
+    focus: function () {
+      if (this.$autocomplete.html() !== '') {
+        this.$autocomplete.show();
+      }
+    },
 
-      blur: function () {
-        this.$autocomplete.hide();
-      },
+    blur: function () {
+      this.$autocomplete.hide();
+    },
 
-      keydown: function (event) {
-        var key = getCharCode(event);
-        switch (getCharCode(event)) {
-          case 13:
-            event.preventDefault();
-            this.mousedown();
-            break;
-          case 40:
-            event.preventDefault();
-            this.walk(1);
-            break;
-          case 38:
-            event.preventDefault();
-            this.walk(-1);
-            break;
-          case 27:
-            this.blur();
-            break;
-        }
-      },
- 
-      keyup: function (event) {
-        if ([13, 40, 38, 27, 37, 39].indexOf(getCharCode(event)) !== -1) {
-          return;
-        }
-        if (!this.$autocomplete) {
-          this.init(); // lazy init
-        }
-        this.setValue(this.format(this.$element.val()));
-        var html = '';
-        $.each(getValues(this.$element.val(), this.options.minDigits, this.options.maxDigits), $.proxy(function (i, val) {
-          html += '<li>' + this.format(val, true) + '</li>';
-        }, this));
-        if (html !== '') {
-          this.$autocomplete.html(html).show();
-        } else {
+    keydown: function (event) {
+      switch (getCharCode(event)) {
+        case 13:
+          event.preventDefault();
+          this.mousedown();
+          break;
+        case 40:
+          event.preventDefault();
+          this.walk(1);
+          break;
+        case 38:
+          event.preventDefault();
+          this.walk(-1);
+          break;
+        case 27:
           this.blur();
-        }
-      },
-  
-      mouseenter: function (event) {
-        this.$autocomplete.find('li').removeClass('selected');
-        $(event.target).addClass('selected');
-      },
-  
-      mouseleave: function (event) {
-        $(event.target).removeClass('selected');
-      },
-  
-      mousedown: function () {
-        this.setValue(this.$autocomplete.find('.selected').html());
+          break;
+      }
+    },
+
+    keyup: function (event) {
+      if ([13, 40, 38, 27, 37, 39].indexOf(getCharCode(event)) !== -1) {
+        return;
+      }
+      if (!this.$autocomplete) {
+        this.init(); // lazy init
+      }
+      this.setValue(this.format(this.$element.val()));
+      var html = '';
+      $.each(
+        getValues(this.$element.val(), this.options.minDigits, this.options.maxDigits),
+        $.proxy(function (i, val) {
+          html += '<li>' + this.format(val, true) + '</li>';
+        }, this)
+      );
+      if (html !== '') {
+        this.$autocomplete.html(html).show();
+      } else {
         this.blur();
-      },
-    
-      setValue: function (val) {
-        if (this.options.removeFixInput) {
-          val = val.replace(this.options.prefix, '').replace(this.options.suffix, '');
-        }
-        if (this.options.unmaskInput) {
-          val = unmask(val);
-        }
-        this.$element.val(val);
-        this.$element.trigger('change');
-      },
-    
-      walk: function (inc) {
-        var selected = this.$autocomplete.find('.selected');
-        if (!selected.length) {
-          this.$autocomplete.find('li:' + (inc > 0 ? 'first' : 'last')).addClass('selected');
-        } else {
-          selected[inc > 0 ? 'next' : 'prev']().addClass('selected');
-          selected.removeClass('selected');
-        }
-      },
-    
-      destroy: function () {
-        this.$element.off();
-        if (this.$autocomplete) {
-          this.$autocomplete.off().remove();
-        }
-      },
-    
-      format: function (val, fix) {
+      }
+    },
+
+    mouseenter: function (event) {
+      this.$autocomplete.find('li').removeClass('selected');
+      $(event.target).addClass('selected');
+    },
+
+    mouseleave: function (event) {
+      $(event.target).removeClass('selected');
+    },
+
+    mousedown: function () {
+      this.setValue(this.$autocomplete.find('.selected').html());
+      this.blur();
+    },
+
+    setValue: function (val) {
+      if (this.options.removeFixInput) {
+        val = val.replace(this.options.prefix, '').replace(this.options.suffix, '');
+      }
+      if (this.options.unmaskInput) {
         val = unmask(val);
-        if (this.options.formatter) {
-          val = this.options.formatter(val, this.options.formatterOptions);
-        }
-        if (fix && val !== '') {
-          val = this.options.prefix + val + this.options.suffix;
-        }
-        return val;
       }
+      this.$element.val(val);
+      this.$element.trigger('change');
+    },
+
+    walk: function (inc) {
+      var selected = this.$autocomplete.find('.selected');
+      if (!selected.length) {
+        this.$autocomplete.find('li:' + (inc > 0 ? 'first' : 'last')).addClass('selected');
+      } else {
+        selected[inc > 0 ? 'next' : 'prev']().addClass('selected');
+        selected.removeClass('selected');
+      }
+    },
+
+    destroy: function () {
+      this.$element.off();
+      if (this.$autocomplete) {
+        this.$autocomplete.off().remove();
+      }
+    },
+
+    format: function (val, fix) {
+      val = unmask(val);
+      if (this.options.formatter) {
+        val = this.options.formatter(val, this.options.formatterOptions);
+      }
+      if (fix && val !== '') {
+        val = this.options.prefix + val + this.options.suffix;
+      }
+      return val;
+    }
   };
 
-  // The jQuery part of the plugin
-  $.fn[pluginName] = function (mixed) {
-    return this.each(function() {
-      var instance = $.data(this, 'plugin_' + pluginName);
-      if (typeof mixed === 'object' && !instance) {
-        $.data(this, 'plugin_' + pluginName, new NumberAutocomplete(this, mixed));
-      } else if (mixed === 'destroy' && instance) {
-        instance.destroy();
-        $.data(this, 'plugin_' + pluginName, false);
-      }
-    });
-  };
-
-
-  // Private methods below
+  // Private methods
 
   function unmask (val) {
     return val.toString().replace(/[^\d]/g, '');
@@ -203,5 +191,23 @@
     }
     return values;
   }
+
+  // The jQuery part of the plugin
+  $.fn[pluginName] = function (mixed) {
+    return this.each(function() {
+      var instance = $.data(this, 'plugin_' + pluginName);
+      if ((!mixed || typeof mixed === 'object') && !instance) {
+        $.data(this, 'plugin_' + pluginName, new NumberAutocomplete(this, mixed));
+      } else if (mixed === 'destroy' && instance) {
+        instance.destroy();
+        $.data(this, 'plugin_' + pluginName, false);
+      }
+    });
+  };
+
+  $[pluginName] = function (options) {
+    // override the defaults for all plugin calls.
+    $.extend(defaults, options || {});
+  };
 
 })(jQuery);
